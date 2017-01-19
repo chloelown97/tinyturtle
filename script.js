09c53a74280912e2362eba6cef769668118983c2
Chloe Lown <chloe.a.lown@gmail.com>
2017-01-19 23:43:49 +0000

    Add TinyTurtle

---
+++ script.js
@@ -0,0 +1,27 @@
+/*global TinyTurtle*/
+TinyTurtle.apply(window);
+forward(50);
+right(90);
+forward(70);
+right(90);
+forward(50);
+right(90);
+forward(70);
+penUp();
+right(90);
+forward (50);
+penDown();
+right(60);
+forward(40);
+right(60);
+forward(40);
+stamp(); 
+penUp();
+right(90);
+forward(60);
+right(90);
+forward(25);
+right(90);
+penDown();
+
+
